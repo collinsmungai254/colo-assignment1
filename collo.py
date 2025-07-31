11993f06d8ec078c99c9e31ce24ee3f4a800e61d
collinsmungai254 <mbuguacollins54@gmail.com>
2025-07-31 23:55:32 +0300

    Add files via upload

---
+++ collo.py
@@ -0,0 +1,13 @@
+#ask the user for two numbers and an operation
+num1=float(input("Enter the first number:"))
+num2=float(input("Enter the second number:"))
+operation=input("Enter an operation(-,=,*,/)")
+
+#perform the operation
+if operation== "+"
+results=num1+num2
+print(f"{num1}+{num2}={results}")
+
+elif operation=="-"
+results=num1+num2
+print (f"{num1}-{num2}={results}")
